0b43d4bb54739d4fe05145ea4204982bf375885d
visualjerk <jb@visualjerk.de>
2022-07-27 23:50:06 +0200

    test unocss

--- components/Header.vue
+++ components/Header.vue
@@ -5,60 +5,9 @@ import { Link } from 'aleph/vue'
 <template>
   <header>
     <div>
-      <h1>
+      <h1 class="p-4 text-2xl">
         <Link to="/">Do</Link>
       </h1>
     </div>
   </header>
 </template>
-
-<style scoped>
-header {
-  width: 100%;
-  height: 80px;
-  position: sticky;
-  top: 0;
-}
-
-header > div {
-  margin: 0 auto;
-  width: 90%;
-  max-width: 900px;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-h1 a {
-  color: #333;
-}
-
-h1 a svg {
-  width: auto;
-  height: 16px;
-}
-
-nav {
-  display: flex;
-  align-items: center;
-  gap: 20px;
-}
-
-nav a {
-  color: #454545;
-}
-
-nav a svg {
-  width: 20px;
-  height: 20px;
-}
-
-a svg {
-  display: inline-flex;
-}
-
-a:hover {
-  color: #000 !important;
-}
-</style>
--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
-import { serve } from "aleph/server";
-import ssr from "aleph/vue-ssr";
-import VueLoader from "aleph/vue-loader";
-import routes from "./routes/_export.ts";
+import presetUno from '@unocss/preset-uno.ts'
+import presetIcons from '@unocss/preset-icons.ts'
+import { serve } from 'aleph/server'
+import ssr from 'aleph/vue-ssr'
+import VueLoader from 'aleph/vue-loader'
+import routes from './routes/_export.ts'
 
 serve({
   baseUrl: import.meta.url,
   router: {
-    glob: "./routes/**/*.{vue,ts}",
+    glob: './routes/**/*.{vue,ts}',
     routes,
   },
   loaders: [new VueLoader()],
+  unocss: {
+    presets: [
+      presetUno(),
+      presetIcons({
+        cdn: 'https://esm.sh/',
+      }),
+    ],
+  },
   ssr,
-});
+})
